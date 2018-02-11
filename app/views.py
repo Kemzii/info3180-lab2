fdcde33a2a91c917887ff0ab81e17e7a248dc18d
kemzii <kemar777@yahoo.com>
2018-02-11 01:36:32 +0000

    create formatdatejoined function

--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+import time
 
 
 ###
@@ -23,6 +24,12 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+    
+    
+@app.route('/profile')
+def profile():
+    """Render fake profile page."""
+    return render_template('profile.html', dtime = format_date_joined())
 
 
 ###
@@ -52,6 +59,13 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
+def format_date_joined():
+    """format date"""
+    dtime = time.strftime("%a, %d %b %y")
+    return dtime
+
 
 
 if __name__ == '__main__':
